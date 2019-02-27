9641a494bc278006871ae60573ea32b93dece663
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-27 16:15:01 -0500

    Created navigation bar for page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FriendsList from './components/FriendsList';
+import './App.css';
+
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			friends: []
+		};
+	}
+
+	componentDidMount() {
+		console.log("CDM is running.");
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<FriendsList />
+				<nav className="nav-bar">
+					<h1 className="nav-header">Tico S. Thepsourinthone</h1>
+					<ul className="nav-links">
+						<a href="#">Home</a>
+						<a href="#">My Friends</a>
+						<a href="#">Add Friend</a>
+					</ul>
+				</nav>
+				<div>
+					<h2>Welcome to My Page!</h2>
+				</div>
+
+
+				<Route 
+					exact
+					path="/friends-list"
+					component={FriendsList}
+				/>
 			</div>
 		);
 	}
