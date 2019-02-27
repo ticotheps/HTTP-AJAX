ee7560a387f2abd465aad2fb22195877c6023031
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-27 16:38:33 -0500

    Got the array from the .map() function inside FriendsList.js to render to the browser

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friend = (props) => {
+    return (
+        <div>
+            <h2>I am one of Tico's Friends!</h2>
+        </div>
+    );
+}
+
+export default Friend;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Friend from './Friend';
 
-const FriendsList = () => {
+const FriendsList = (props) => {
     return (
         <div>
             <h1>These are all the friends in my #CodeFam!</h1>
+            {props.friends.map(friend => (
+                <Friend />
+            ))}
         </div>
     );
 }
