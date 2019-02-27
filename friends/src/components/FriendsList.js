8c87f549bacf72c67cc0836728a037add7c8d168
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-27 14:27:32 -0500

    Created 'components' folder and created <FriendsList /> component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = () => {
+    return (
+        <div>
+            <h1>This is my #CodeFam!</h1>
+        </div>
+    );
+}
+
+export default FriendsList;
