1eb9036134de7b2a415ca0f6d44d91c6410a0cf9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-28 16:52:01 -0500

    Modified <input> elements in <form> to have proper attributes

--- friends/src/components/NewFriendForm.js
+++ friends/src/components/NewFriendForm.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class NewFriendForm extends React.Component {
     state = {
-
+        friend: {
+            name: "",
+            id: "",
+            age: "",
+            email: ""
+        }
     }
 
     render() {
@@ -12,15 +17,33 @@ class NewFriendForm extends React.Component {
                 <form className="new-friend-form">
                     <div className="friend-name-input-area">
                         <label for="friend-name">Name: </label>
-                        <input type="text" id="friend-name" />
+                        <input 
+                            id="friend-name"
+                            type="text" 
+                            name="name"
+                            placeholder="Name"
+                            value={this.state.friend.name}
+                        />
                     </div>
                     <div className="friend-age-input-area">
                         <label for="friend-age">Age: </label>
-                        <input type="text" id="friend-age" />
+                        <input 
+                            id="friend-age"
+                            type="number" 
+                            name="age"
+                            placeholder="Age"
+                            value={this.state.friend.age}
+                        />
                     </div>
                     <div className="friend-email-input-area">
                         <label for="friend-email">Email: </label>
-                        <input type="text" id="friend-email" />
+                        <input 
+                            id="friend-email"
+                            type="text" 
+                            name="email"
+                            placeholder="Email"
+                            value={this.state.friend.email}
+                        />
                     </div>
                     <button className="save-button">Save</button>
                 </form>
